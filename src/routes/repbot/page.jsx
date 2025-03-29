a6c743d56d214ed4ea176c88980411fadb62ce40
rapidroutines <rapidroutines@gmail.com>
2025-03-28 22:54:45 -0400

    Update page.jsx

--- src/routes/repbot/page.jsx
+++ src/routes/repbot/page.jsx
@@ -13,10 +13,6 @@ const SUPPORTED_EXERCISES = [
 ];
 
 const RepBotPage = () => {
-  const handleLaunchClick = () => {
-    window.location.href = "https://rapidroutines.org/repbot/";
-  };
-
   return (
     <div className="flex flex-col items-center gap-y-6">
       {/* Welcoming Header */}
@@ -49,13 +45,13 @@ const RepBotPage = () => {
         
         {/* Launch Button */}
         <div className="flex justify-center mt-6">
-          <button 
-            onClick={handleLaunchClick}
+          <a 
+            href="https://rapidroutines.org/repbot/"
             className="inline-flex items-center justify-center rounded-lg bg-[#1e628c] text-white px-8 py-3 font-medium text-lg hover:bg-[#1a567c] transition-colors"
           >
             <Camera size={20} className="mr-2" />
             Launch RepBot
-          </button>
+          </a>
         </div>
       </div>
 
